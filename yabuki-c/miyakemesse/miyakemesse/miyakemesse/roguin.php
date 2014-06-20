86cc1c6b7dc8a0c1d3065783c0ecbac9f61b04ea
Miurataisuke <ompend@gmai.com>
2014-06-20 15:43:59 +0900

    test

---
+++ yabuki-c/miyakemesse/miyakemesse/miyakemesse/roguin.php
@@ -0,0 +1,12 @@
+<html>
+       <head>
+        <meta charset="UTF-8">
+        <title>三宅メッセンジャー</title>
+    </head>
+    <body>
+        <CENTER>
+            <p><font size="7">
+                三宅メッセンジャー</font></p>
+        </CENTER>
+    </body>
+</html>>
