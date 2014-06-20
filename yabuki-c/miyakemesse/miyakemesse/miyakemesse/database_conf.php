4cf09b2dde0aca5ba584609ac44604f63d79a685
Miurataisuke <ompend@gmai.com>
2014-06-20 14:36:27 +0900

    Revert "test"
    This reverts commit 6c6efc7728409581f845d2fa6cea5307efb190a1.

--- yabuki-c/miyakemesse/miyakemesse/miyakemesse/database_conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-// データベース設定
-$dbServer = '127.0.0.1';
-$dbUser   = 'Miura';
-$dbPass   = 'ybkc';
-$dbName   = 'mydb';
-
-# MySQL用のDSN文字列です。
-$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
