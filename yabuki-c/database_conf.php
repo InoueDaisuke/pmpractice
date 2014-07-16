ae31647e5f5b089618685275f4a74392cf66e654
InoueDaisuke <hextech.master.yi@gmail.com>
2014-07-16 20:07:50 +0900

    Update database_conf.php

--- yabuki-c/database_conf.php
+++ yabuki-c/database_conf.php
@@ -1,9 +1,17 @@
 <?php
 // データベース設定
-$dbServer = '127.0.0.1';
-$dbUser   = 'root';
-$dbPass   = 'ybkc';
-$dbName   = 'mydb';
+$dbServer = '175.184.35.36';
+$dbUser   = 'ct4ttvj_azd';
+$dbPass   = '8OT2LW1q';
+$dbName   = 'ct4ttvj_azd';
 
 # MySQL用のDSN文字列です。
-$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
+$dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8',
+
+ $_SERVER["C4SA_MYSQL_HOST"],
+
+ $_SERVER["C4SA_MYSQL_DB"]);
+
+$db = new PDO($dsn, $dbUser, $dbPass);
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
