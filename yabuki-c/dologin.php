3dc2a12c39ce8919eca0a1dfa48e5f6bdbc6b393
InoueDaisuke <hextech.master.yi@gmail.com>
2014-07-16 20:11:03 +0900

    Update dologin.php

--- yabuki-c/dologin.php
+++ yabuki-c/dologin.php
@@ -12,9 +12,7 @@ $userid=$_POST['userid'];
 $password=$_POST['password'];
 //データベースに接続
 require_once 'database_conf.php';
-$db = new PDO($dsn, $dbUser, $dbPass);
-$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 
 //検索
 $sql = "select * from users where userid=:userid and password=:password";
